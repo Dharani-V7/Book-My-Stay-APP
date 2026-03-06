808617d9591effb85df55a4ff52ed25d547c89cb
Dharani-V7 <dv5589@srmist.edu.in>
2026-03-06 09:40:26 +0530

    UC2:Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,16 +1,86 @@
-public class BookMyStayApp {
-    public static void main(String[] args) {
+       abstract class Room {
 
-        // Display welcome message
-        System.out.println("=================================");
-        System.out.println("     Welcome to Book My Stay     ");
-        System.out.println("     Hotel Booking System        ");
-        System.out.println("        Version 1.0              ");
-        System.out.println("=================================");
+        protected int beds;
+        protected int size;
+        protected double price;
 
-        // End message
-        System.out.println("Application started successfully.");
-        System.out.println("Thank you for using Book My Stay!");
+        public Room(int beds, int size, double price) {
+            this.beds = beds;
+            this.size = size;
+            this.price = price;
+        }
+
+        public void displayRoomDetails() {
+            System.out.println("Beds: " + beds);
+            System.out.println("Size: " + size + " sq.ft");
+            System.out.println("Price: $" + price);
+        }
+
+        abstract String getRoomType();
     }
-}
-}
+
+    class SingleRoom extends Room {
+
+        public SingleRoom() {
+            super(1, 200, 100);
+        }
+
+        public String getRoomType() {
+            return "Single Room";
+        }
+    }
+
+    class DoubleRoom extends Room {
+
+        public DoubleRoom() {
+            super(2, 350, 180);
+        }
+
+        public String getRoomType() {
+            return "Double Room";
+        }
+    }
+
+    class SuiteRoom extends Room {
+
+        public SuiteRoom() {
+            super(3, 600, 400);
+        }
+
+        public String getRoomType() {
+            return "Suite Room";
+        }
+    }
+
+    public class BookMyStayApp {
+
+        public static void main(String[] args) {
+
+            System.out.println("=================================");
+            System.out.println("   Book My Stay - Version 2.1");
+            System.out.println("   Room Types & Availability");
+            System.out.println("=================================");
+
+            Room single = new SingleRoom();
+            Room doubleRoom = new DoubleRoom();
+            Room suite = new SuiteRoom();
+
+            int singleAvailable = 5;
+            int doubleAvailable = 3;
+            int suiteAvailable = 2;
+
+            System.out.println("\nRoom Type: " + single.getRoomType());
+            single.displayRoomDetails();
+            System.out.println("Available Rooms: " + singleAvailable);
+
+            System.out.println("\nRoom Type: " + doubleRoom.getRoomType());
+            doubleRoom.displayRoomDetails();
+            System.out.println("Available Rooms: " + doubleAvailable);
+
+            System.out.println("\nRoom Type: " + suite.getRoomType());
+            suite.displayRoomDetails();
+            System.out.println("Available Rooms: " + suiteAvailable);
+
+            System.out.println("\nApplication terminated successfully.");
+        }
+    }
